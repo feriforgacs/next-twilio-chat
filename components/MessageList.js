58c29ea6d12826beb2d20e2e73595c666c49725c
Ferenc Forgacs <forgacsf@gmail.com>
2022-06-14 18:07:18 +0200

    [wip]

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,3 +1,49 @@
-export default function MessageList({ token }) {
-	return <div>MessageList - {token}</div>;
+import { useState, useEffect } from "react";
+import { Client } from "@twilio/conversations";
+
+export default function MessageList({ token, identity, conversationSid }) {
+	const [client, setClient] = useState();
+	const [message, setMessage] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		if (token) {
+			const client = new Client(token);
+
+			client.on("stateChanged", async (state) => {
+				if (state === "initialized") {
+					setClient(client);
+
+					const conversation = await client.getConversationBySid(conversationSid);
+
+					console.log(conversation);
+				}
+			});
+		}
+	}, [token, identity, conversationSid]);
+
+	const sendMessage = async (e) => {
+		setLoading(true);
+		e.preventDefault();
+		setLoading(false);
+	};
+
+	return (
+		<div>
+			<h1>Messages</h1>
+			<ul>
+				<li>Message 1</li>
+				<li>Message 2</li>
+				<li>Message 3</li>
+			</ul>
+			{client && (
+				<form onSubmit={(e) => sendMessage(e)}>
+					<input type={"text"} value={message} onChange={(e) => setMessage(e.target.value)} />
+					<button type="submit" disabled={loading}>
+						{loading ? "Sending" : "Send message"}
+					</button>
+				</form>
+			)}
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,22 @@ export default function Home() {
 	const [identity, setIdentity] = useState("");
 	const [token, setToken] = useState();
 	const [loading, setLoading] = useState(false);
+	const [conversationSid, setConversationSid] = useState();
 
 	const getToken = async (e) => {
 		e.preventDefault();
 		setLoading(true);
 		// create new conversation
 		const conversation = await axios.get("/api/createConversation");
-		const sid = conversation.data.conversation.sid;
+		const { chatServiceSid, sid } = conversation.data.conversation;
+		setConversationSid(sid);
 
 		// add participant to conversation
-		const participant = await axios.get(`/api/addParticipant?sid=${sid}&identity=${identity}`);
+		await axios.get(`/api/addParticipant?sid=${sid}&identity=${identity}`);
 
 		// get token for participant
-		const token = await axios.get(`/api/getToken?sid=${sid}&identity=${identity}`);
-		setToken(token);
+		const token = await axios.get(`/api/getToken?sid=${chatServiceSid}&identity=${identity}`);
+		setToken(token.data.token);
 
 		setLoading(false);
 	};
@@ -29,7 +31,7 @@ export default function Home() {
 		<main>
 			<h1>Next.js - Twilio Conversations</h1>
 			{!token && <LogIn identity={identity} setIdentity={setIdentity} getToken={getToken} loading={loading} />}
-			{token && <MessageList token={token} />}
+			{token && <MessageList token={token} conversationSid={conversationSid} />}
 		</main>
 	);
 }
